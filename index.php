51550fb49779a9e6762732e012ebd64b771eb4bd
Shubham Goyal <mail.goyalshubham@gmail.com>
2014-05-27 15:34:26 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	date_default_timezone_set('Asia/Kolkata');
+ ?>
+<html>
+	<head>
+		<title>Heroku deployment demo</title>
+	</head>
+	<body>
+		<h1>Deployed on Heroku</h1>
+		<h3><?php echo date('l jS \of F Y'); ?></h3>
+	</body>
+</html>
